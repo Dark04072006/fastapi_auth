78e507949acf7a09ba5cb4a9a47d056bca5d301c
Dark04072006 <139165232+Dark04072006@users.noreply.github.com>
2023-07-31 17:24:42 +0300

    Update permissions.py

--- app/auth/permissions.py
+++ app/auth/permissions.py
@@ -1,11 +1,1 @@
-# from fastapi import Depends, Request
-# from sqlalchemy.ext.asyncio import AsyncSession
 
-# from app.auth.services import crud_operations
-# from core.database.utils import get_session
-
-
-# async def only_current_user_perm(
-#         session: AsyncSession = Depends(get_session), request: Request):
-#     token = request.headers.get('Authorization').split()[1]
-    
